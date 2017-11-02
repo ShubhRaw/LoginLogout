cc655206b95b56f0c2efaa2154cf5b8be9baeda3
ShubhRaw <30543191+ShubhRaw@users.noreply.github.com>
2017-11-02 15:44:39 +0530

    Update UserDAOHibImp.java

--- src/com/onmobile/hib/UserDAOHibImp.java
+++ src/com/onmobile/hib/UserDAOHibImp.java
@@ -3,6 +3,7 @@ package com.onmobile.hib;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+//live
 
 import com.onmobile.shubham.User;
 import com.onmobile.shubham.UserDAO;
